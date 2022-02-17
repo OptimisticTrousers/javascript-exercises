22c8711a40c85c369a6a9a892e7d271182406076
OptimisticTrousers <tony.isern@yahoo.com>
2022-02-16 23:15:16 -0500

    Passed first test for exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,24 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...removedElementsFromArray) {
+
+
+    //return array.map(x, index => x == functionArgument.map().includes() ? array.splice(index, 1) : x)
+
+
+    //finalArray.some((item, index) => functionArgument.includes(item) ? finalArray = finalArray.splice(index, 1) : [])
+
+
+    for(let i = 0; i < array.length; i++){
+console.log(i)
+
+        if(removedElementsFromArray.includes(array[i])){
+           array.splice(i, 1) 
+    }
+
+    }
+
+
+    return array 
+
 
 };
 
