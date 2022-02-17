0c4e7afa468ec9d185d1018d3c1ad78b29c8fb0d
OptimisticTrousers <tony.isern@yahoo.com>
2022-02-17 14:42:20 -0500

    Finishing sum and subtraction methods

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,24 +1,64 @@
-const add = function() {
+const add = function(...numbers) {
+  
+  let sum = 0
+
+  for(let i = 0; i < numbers.length; i++){
+    sum += numbers[i]
+  }
+
+  return sum
+  
+};
+
+const subtract = function(...numbers) {
+  
+  let subtraction = numbers[0] 
+
+  for(let i = 1; i < numbers.length; i++){
+    subtraction = subtraction - numbers[i]
+  }
+
+  return subtraction 
 	
 };
 
-const subtract = function() {
+const sum = function(numbers) {
+	
+  let sum = 0
+
+  for(let i = 0; i < numbers.length; i++){
+    sum += numbers[i]
+  }
+
+  return sum
+
+};
+
+const multiply = function(numbers) {
+
+  let product = 0
+
+  for(let i = 0; i < numbers.length; i++){
+    product *= numbers[i]
+  }
+
+  return product 
+
+};
+
+const power = function(number, power) {
+
+  return Math.pow(number, power)
 	
 };
 
-const sum = function() {
-	
-};
+const factorial = function(number) {
 
-const multiply = function() {
+  if(number == 0){
+    return 1
+  }
 
-};
-
-const power = function() {
-	
-};
-
-const factorial = function() {
+  return number * factorial(number-1)
 	
 };
 
