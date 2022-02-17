8a815dbca4dc63e1c786d4911f9df718b702a362
OptimisticTrousers <tony.isern@yahoo.com>
2022-02-17 15:18:18 -0500

    Solving exercise 09 by using regex

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,18 @@
-const palindromes = function () {
+const palindromes = function (string) {
 
+    const regex = /[A-Za-z]/gi
+    
+    let unreversedArray = string.toLowerCase().match(regex)
+
+    let stringArray = string.split('').reverse().join('').toLowerCase()
+
+    let noPunctuation = stringArray.match(regex)
+
+    if(noPunctuation.join('') == unreversedArray.join('')){
+        return true
+    }
+
+    return false
 };
 
 // Do not edit below this line
