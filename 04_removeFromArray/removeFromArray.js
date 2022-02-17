e2c689042c14936f4a79ef6a878f19f2e5b61d55
OptimisticTrousers <tony.isern@yahoo.com>
2022-02-17 10:20:43 -0500

    Finished exercise 04 and all tests passed

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,17 +1,17 @@
 const removeFromArray = function(array, ...removedElementsFromArray) {
 
-
     //return array.map(x, index => x == functionArgument.map().includes() ? array.splice(index, 1) : x)
 
+    let finalArray = array
 
     //finalArray.some((item, index) => functionArgument.includes(item) ? finalArray = finalArray.splice(index, 1) : [])
 
 
     for(let i = 0; i < array.length; i++){
-console.log(i)
 
         if(removedElementsFromArray.includes(array[i])){
            array.splice(i, 1) 
+           i--
     }
 
     }
