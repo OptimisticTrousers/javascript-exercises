d514ae52a03b42df50aafc9208780bdf74b00174
OptimisticTrousers <tony.isern@yahoo.com>
2022-03-23 23:02:03 -0400

    Finished exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,4 +1,31 @@
-const findTheOldest = function() {
+const findTheOldest = function(objects) {
+
+    const currentYear = new Date().getFullYear();
+
+    //return objects.reduce((accumulator, currentValue) => {
+
+        //let yearOfDeath = currentValue.yearOfDeath ? currentValue.yearOfDeath : currentYear;
+
+        //accumulator.push(currentValue);
+        
+        //yearOfDeath - currentValue.yearOfBirth; 
+
+        //return 
+
+    //}, {})
+    
+    const sortedObjects = objects.sort((a,b) => {
+        
+
+        const personDeathA = a.yearOfDeath ? a.yearOfDeath : currentYear;
+
+        const personDeathB = b.yearOfDeath ? b.yearOfDeath : currentYear;
+
+        return (personDeathA - a.yearOfBirth) > (personDeathB - b.yearOfBirth) ? -1 : 1;
+
+    })
+
+    return sortedObjects[0];
 
 };
 
