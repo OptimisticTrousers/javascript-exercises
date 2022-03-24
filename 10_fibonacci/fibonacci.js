86c26aaa538900a170791666c91b01dceb278545
OptimisticTrousers <tony.isern@yahoo.com>
2022-03-23 22:15:21 -0400

    Finished exercise 10 using a for loop

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,35 @@
-const fibonacci = function() {
+const fibonacci = function(numberSequence) {
 
+    if(numberSequence == 1){
+        return 1;
+    }
+    else if(numberSequence < 0){
+        return "OOPS"
+    }
+
+    let array = [1, 1]; 
+
+    let find = 0;
+
+    //(1, 1, 2, 3)
+
+    //if numberSequence == 3, 1 2 3
+
+    //its going to push (1, )
+
+    for(let i = 1; i <= numberSequence-1; i++){
+
+        if(i === numberSequence-1){
+            find = array[i];
+        }
+
+        previousValue = array[i-1];
+        currentValue = array[i];
+
+        array.push(previousValue + currentValue);
+    }
+
+    return find;
 };
 
 // Do not edit below this line
